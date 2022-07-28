06d7aefa123f95d87b543f7cbcd711771cfa2777
Richard Odell <richardodell@Richards-MBP.attlocal.net>
2022-07-28 00:58:55 -0400

    end of day backup

--- schema.sql
+++ schema.sql
@@ -31,7 +31,6 @@ CREATE TABLE salaries (
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
 
@@ -39,10 +38,7 @@ CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
-
+	to_date DATE NOT NULL,	
 );
 
 CREATE TABLE dept_emp (
@@ -53,5 +49,3 @@ CREATE TABLE dept_emp (
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no,dept_no)
-);
-
